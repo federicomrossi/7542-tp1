f64bdf577a447514e824ad94bdd3f40198b4acb0
Federico Rossi <federicomrossi@gmail.com>
2013-03-16 17:04:38 -0300

    Solucionado error de calculo  al no superar tolerancias

--- Codigo/multas.c
+++ Codigo/multas.c
@@ -36,7 +36,7 @@ int main(int argc, char **argv)
  	int consumo_tipico = 0; 
 	int duracion_interrupcion;
 	int cant_interrupciones = 0;
-	int minutos_multables = 0;
+	int minutos_acumulados = 0;
 
 	// Flags para deteccion de tolerancias superadas
 	_Bool flag_tol_frecuencia = 0;
@@ -55,7 +55,7 @@ int main(int argc, char **argv)
 
  	// inicializamos variables de procesamiento
  	cant_interrupciones = 0;
- 	minutos_multables = 0;
+ 	minutos_acumulados = 0;
 
 
  	while(fgets(buffer, 50, fp))
@@ -66,10 +66,15 @@ int main(int argc, char **argv)
 		if((cliente != cliente_tmp) && (cliente_tmp !=0))
 		{
 			// Calculamos multa correspondiente al cliente (cliente_tmp)
-			printf("Cliente: %d\n", cliente_tmp);
-			printf("Interrupciones: %d\n", cant_interrupciones);
-			printf("Acumulado: %d\n", minutos_multables);
-			printf("MULTA: %d\n\n", calculador_multa(consumo_tipico, x_p, minutos_multables));
+			// printf("Cliente: %d\n", cliente_tmp);
+			// printf("Interrupciones: %d\n", cant_interrupciones);
+			// printf("Acumulado: %d\n", minutos_acumulados);
+			// printf("MULTA: %d\n\n", calculador_multa(consumo_tipico, x_p, minutos_acumulados));
+						
+			if((!flag_tol_frecuencia) && (!flag_tol_duracion_acumulada))
+				minutos_acumulados = 0;
+
+			printf("%d:%d\n", cliente_tmp, calculador_multa(consumo_tipico, x_p, minutos_acumulados));
 
 			// Establecemos el nuevo cliente como actual
 			cliente_tmp = cliente;
@@ -78,7 +83,7 @@ int main(int argc, char **argv)
 			flag_tol_frecuencia = 0;
 			flag_tol_duracion_acumulada = 0;
 		 	cant_interrupciones = 0;
-		 	minutos_multables = 0;
+		 	minutos_acumulados = 0;
 		}
 
 		// Sensa al primer cliente de la primer interrupcion
@@ -94,38 +99,38 @@ int main(int argc, char **argv)
 		if(!filtro_es_interrupcion_momentanea(x_m, duracion_interrupcion))
 		{
 			cant_interrupciones++;
-			minutos_multables += duracion_interrupcion;
+			minutos_acumulados += duracion_interrupcion;
 
 			if ((flag_tol_frecuencia) || (flag_tol_duracion_acumulada))
 				continue;
 
 			// Se supera la tolerancia por frecuencia primero
 			if ((!filtro_tolerancia_interrupcion_por_frecuencia(x_f, cant_interrupciones)) &&
-				(filtro_tolerancia_interrupcion_por_duracion_acumulada(x_d, minutos_multables)))
+				(filtro_tolerancia_interrupcion_por_duracion_acumulada(x_d, minutos_acumulados)))
 			{
 				flag_tol_frecuencia = 1;
-				minutos_multables = 0;
-				minutos_multables += duracion_interrupcion;
-				printf("Se supero TOLERANCIA POR FRECUENCIA\n");
+				minutos_acumulados = 0;
+				minutos_acumulados += duracion_interrupcion;
+				////printf("Se supero TOLERANCIA POR FRECUENCIA\n");
 			}
 			// Se supera la tolerancia por duración acumulada primero
 			else if ((filtro_tolerancia_interrupcion_por_frecuencia(x_f, cant_interrupciones)) &&
-				(!filtro_tolerancia_interrupcion_por_duracion_acumulada(x_d, minutos_multables)))
+				(!filtro_tolerancia_interrupcion_por_duracion_acumulada(x_d, minutos_acumulados)))
 			{
 				flag_tol_duracion_acumulada = 1;
-				minutos_multables -= x_d;
-				printf("Se supero TOLERANCIA POR DURACION ACUMULADA\n");
+				minutos_acumulados -= x_d;
+				////printf("Se supero TOLERANCIA POR DURACION ACUMULADA\n");
 			}
 			// Se superan las tolerancias por frecuencia y por duración
 			// acumulada al mismo tiempo
 			else if ((!filtro_tolerancia_interrupcion_por_frecuencia(x_f, cant_interrupciones)) &&
-				(!filtro_tolerancia_interrupcion_por_duracion_acumulada(x_d, minutos_multables)))
+				(!filtro_tolerancia_interrupcion_por_duracion_acumulada(x_d, minutos_acumulados)))
 			{
 				flag_tol_frecuencia = 1;
 				flag_tol_duracion_acumulada = 1;
-				minutos_multables = 0;
-				minutos_multables += duracion_interrupcion;
-				printf("Se superaron AMBAS TOLERANCIAS A LA VEZ\n");
+				minutos_acumulados = 0;
+				minutos_acumulados += duracion_interrupcion;
+				////printf("Se superaron AMBAS TOLERANCIAS A LA VEZ\n");
 			}
 		}
  	}
@@ -137,11 +142,15 @@ int main(int argc, char **argv)
 
 	
 	// Calculamos multa correspondiente al cliente (cliente_tmp)
-	printf("Cliente: %d\n", cliente_tmp);
-	printf("Interrupciones: %d\n", cant_interrupciones);
-	printf("Acumulado: %d\n", minutos_multables);
-	printf("MULTA: %d\n\n", calculador_multa(consumo_tipico, x_p, minutos_multables));
+	//printf("Cliente: %d\n", cliente_tmp);
+	//printf("Interrupciones: %d\n", cant_interrupciones);
+	//printf("Acumulado: %d\n", minutos_acumulados);
+	//printf("MULTA: %d\n\n", calculador_multa(consumo_tipico, x_p, minutos_acumulados));
+ 	
+	if((!flag_tol_frecuencia) && (!flag_tol_duracion_acumulada))
+		minutos_acumulados = 0;
 
+ 	printf("%d:%d\n", cliente_tmp, calculador_multa(consumo_tipico, x_p, minutos_acumulados));
 	
 	// Cerramos el archivo
 	fclose(fp);
